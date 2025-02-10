acaca61f34dcbf9929c8b3c31be4b27b05f52364
thibaultmrn <thibault.mariani@student.hepl.be>
2025-02-10 13:30:22 +0100

    classe coordonnee

---
+++ MyShapeLibrary/Coordonnee.cs
@@ -0,0 +1,35 @@
+﻿namespace MyShapeLibrary
+{
+    public class Coordonnee
+    {
+        private int X;
+        private int Y;
+
+        //Propriété X
+        public int x
+        {
+            get { return X; }
+            set { X = value; }
+        }
+        //Propriété Y
+        public int y
+        {
+            get { return Y; }
+            set { Y = value; }
+        }
+
+        //Constructeur d'initialisation
+        public Coordonnee(int x,int y)
+        {
+            Y = y;
+            X = x;
+        }
+
+        public Coordonnee() : this(0,0) { }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+}
